72a9bff2ce3b332ad9928065bfa2cc244cc624f4
Henrique Hida <78568172+HenriqueHida@users.noreply.github.com>
2024-04-07 18:42:33 -0300

    Update scripts.js
    adição da seção de artefatos

--- github-pages/static/js/scripts.js
+++ github-pages/static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publicacoes', 'equipe']
+const section_names = ['home', 'publications', 'artefatos',  'equipe']
 
 
 window.addEventListener('DOMContentLoaded', event => {
