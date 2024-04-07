7da3f6ded339bb8490467670608a451610b43d5f
HenriqueHida <henriquehid@gmail.com>
2024-04-07 20:53:54 -0300

    Adicao Glossario

--- github-pages/static/js/scripts.js
+++ github-pages/static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'artefatos',  'equipe']
+const section_names = ['home', 'glossario','artefatos', 'equipe']
 
 
 window.addEventListener('DOMContentLoaded', event => {
