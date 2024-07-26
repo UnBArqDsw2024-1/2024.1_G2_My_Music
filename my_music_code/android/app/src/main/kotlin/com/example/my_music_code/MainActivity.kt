cb607499a1c8e8a947cb57d7a8a792bb6599ef2a
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-07-26 08:33:20 -0300

    Init do código do projeto em flutter
    Criação do código base do aplicativo

---
+++ my_music_code/android/app/src/main/kotlin/com/example/my_music_code/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_music_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ my_music_code/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyMusic',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage()
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
