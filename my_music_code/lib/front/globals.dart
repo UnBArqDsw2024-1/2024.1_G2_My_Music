4034a9c033ea748811147ea5f6ad19f10d5805cc
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-01 13:57:57 -0300

    Clear backend test + loadingDialog

--- my_music_code/lib/front/globals.dart
+++ my_music_code/lib/front/globals.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_music_code/front/style.dart';
 
+Map<String,String> errorMap = {
+  'channel-error':'preencha todos os campos!',
+  'invalid-email':'email inválido!',
+  'invalid-credential':'credenciais inválidas!',
+  'weak-passowrd':'senha fraca!',
+};
+
 void errorDialogMessage(BuildContext context, String error, {String title = "Erro de autenticação"}){
   // Mostra a mensagem de erro
   showDialog(
@@ -17,6 +24,15 @@ void errorDialogMessage(BuildContext context, String error, {String title = "Err
           ),
         ],
       );
+    }
+  );
+}
+
+void loadingDialog(BuildContext context, {Widget child = const Center(child: CircularProgressIndicator())}){
+  showDialog(
+    context: context,
+    builder: (context) {
+      return child;
     }
   );
 }
