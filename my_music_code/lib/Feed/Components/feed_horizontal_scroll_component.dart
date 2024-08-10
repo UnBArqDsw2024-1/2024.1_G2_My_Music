0fe9cce3c2ca636787899998302812683b7f58e2
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-09 22:32:17 -0300

    Criando e utilizando componentes #3

---
+++ my_music_code/lib/Feed/Components/feed_horizontal_scroll_component.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:my_music_code/Feed/Components/responsive_container.dart';
+import 'package:my_music_code/Feed/Components/responsive_text.dart';
+import 'package:my_music_code/Globals/size_config.dart';
+
+class FeedHorizontalScrollComponent extends StatefulWidget {
+  const FeedHorizontalScrollComponent({super.key, required this.title});
+  final String title;
+  @override
+  State<FeedHorizontalScrollComponent> createState() => _FeedHorizontalScrollComponentState();
+}
+
+class _FeedHorizontalScrollComponentState extends State<FeedHorizontalScrollComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: responsiveFigmaHeight(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ResponsiveText(
+              text: widget.title,
+              fontColor: Colors.white,
+              fontWeight: FontWeight.w700,
+              fontSize: 16,
+              padding: EdgeInsets.only(left: responsiveFigmaWidth(23))),
+          ResponsiveContainer(height: 8),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                ResponsiveContainer(width: 23),
+                for (int i = 0; i < 10; i++)
+                  Padding(
+                    padding: EdgeInsets.only(right: responsiveFigmaWidth(10)),
+                    child: ResponsiveContainer(
+                      height: 100,
+                      width: 100,
+                      isCubic: true,
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                  ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- my_music_code/lib/Feed/feed_page.dart
+++ my_music_code/lib/Feed/feed_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_music_code/Feed/Components/music_playlist_feed_component.dart';
-import 'package:my_music_code/Feed/Components/responsive_container.dart';
-import 'package:my_music_code/Feed/Components/responsive_text.dart';
-import 'package:my_music_code/Globals/size_config.dart';
+import 'package:my_music_code/Feed/Components/feed_horizontal_scroll_component.dart';
+import 'package:my_music_code/Feed/Components/feed_music_grid.dart';
+import 'package:my_music_code/Feed/Components/new_music_release.dart';
 import 'package:my_music_code/Globals/style.dart';
 
 class FeedPage extends StatefulWidget {
@@ -16,196 +15,21 @@ class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
         backgroundColor: backgroundColor,
-        appBar: AppBar(
-          backgroundColor: backgroundColor,
-          title: Text("Feed", style: TextStyle(color: Colors.white)),
+        title: Text("Feed", style: TextStyle(color: Colors.white)),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            FeedMusicGrid(),
+            NewMusicRelease(),
+            FeedHorizontalScrollComponent(title: "Top World Albuns"),
+            FeedHorizontalScrollComponent(title: "Tocadas recentemente"),
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: responsiveFigmaHeight(12)),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center, 
-                  children: const [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MusicPlaylistFeedComponent(),
-                      ResponsiveContainer(width: 10),
-                      MusicPlaylistFeedComponent(),
-                    ],
-                  ),
-                  ResponsiveContainer(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MusicPlaylistFeedComponent(),
-                      ResponsiveContainer(width: 10),
-                      MusicPlaylistFeedComponent(),
-                    ],
-                  ),
-
-                  ResponsiveContainer(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MusicPlaylistFeedComponent(),
-                      ResponsiveContainer(width: 10),
-                      MusicPlaylistFeedComponent(),
-                    ],
-                  ),
-                  ResponsiveContainer(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MusicPlaylistFeedComponent(),
-                      ResponsiveContainer(width: 10),
-                      MusicPlaylistFeedComponent(),
-                    ],
-                  ),
-                ]),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 23),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        ResponsiveContainer(
-                          height: 60,
-                          width: 60,
-                          isCubic: true,
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        ResponsiveContainer(width: 5),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            ResponsiveText(text: "Novo lançamento",fontColor: Color(0xff979797),fontSize: 12),
-                            ResponsiveText(text: "Artista",fontWeight: FontWeight.w500,fontColor: Colors.white,fontSize: 16),
-                          ],
-                        )
-                      ],
-                    ),
-                    ResponsiveContainer(height: 18),
-                    ResponsiveContainer(
-                      height: 130,
-                      width: 326,
-                      color: DefaultPlaceholder.backgroundColor,
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      child: Stack(
-                        children: [
-                          Row(
-                            children: const [
-                              ResponsiveContainer(
-                                height: 130,
-                                width: 130,
-                                isCubic: true,
-                                color: Colors.white,
-                                borderRadius: BorderRadius.horizontal(left: Radius.circular(15)),
-                              ),
-                              ResponsiveContainer(width: 12),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  ResponsiveContainer(height: 12),
-                                  ResponsiveText(
-                                    text: "Minha música nova", 
-                                    fontColor: Colors.white, 
-                                    fontSize: 16
-                                  ),
-                                  ResponsiveText(
-                                    text: "Single by DJ Onga",
-                                    fontColor: Color(0xff979797), 
-                                    fontSize: 12,
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                          Positioned(
-                            right: 10,
-                            bottom: 10,
-                            child: IconButton.filled(
-                              style: IconButton.styleFrom(
-                                backgroundColor: Colors.white,
-                              ),
-                              iconSize: 36,
-                              onPressed: (){}, 
-                              icon: Icon(Icons.play_arrow_rounded, color: secondaryColor,size: 36,),
-                            )
-                          )
-                        ],
-                      
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: responsiveFigmaHeight(10)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ResponsiveText(text:"Top World Albuns",fontColor: Colors.white,fontWeight: FontWeight.w700, fontSize: 16, padding: EdgeInsets.only(left: responsiveFigmaWidth(23))),
-                    ResponsiveContainer(height: 8),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          ResponsiveContainer(width: 23),
-                          for(int i = 0; i < 10; i++)
-                            Padding(
-                              padding: EdgeInsets.only(right: responsiveFigmaWidth(10)),
-                              child: ResponsiveContainer(
-                                height: 100,
-                                width: 100,
-                                isCubic: true,
-                                borderRadius: BorderRadius.circular(15),
-                                color: Colors.white,
-                              ),
-                            ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ResponsiveText(text: "Tocadas recentemente",fontColor: Colors.white,fontWeight: FontWeight.w700, fontSize: 16, padding: EdgeInsets.only(left: responsiveFigmaWidth(23))),
-                  ResponsiveContainer(height: 8),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ResponsiveContainer(width: 23),
-                        for(int i = 0; i < 5; i++)
-                          Padding(
-                            padding: EdgeInsets.only(right: responsiveFigmaWidth(10)),
-                            child: ResponsiveContainer(
-                              height: 100,
-                              width: 100,
-                              isCubic: true,
-                              borderRadius: BorderRadius.circular(15),
-                              color: Colors.white,
-                            ),
-                          ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
-        ));
+      )
+    );
   }
 }
