242bb8b853b87aa2e05aa04642c6428bed1e54c7
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-09 22:17:42 -0300

    Atualizando campos da tela com espaçamento correto

--- my_music_code/lib/Feed/feed_page.dart
+++ my_music_code/lib/Feed/feed_page.dart
@@ -24,11 +24,12 @@ class _FeedPageState extends State<FeedPage> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              ResponsiveContainer(
-                width: double.infinity,
-                height: responsiveFigmaHeight(275),
-                color: backgroundColor,
-                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: responsiveFigmaHeight(12)),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center, 
+                  children: const [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -46,6 +47,7 @@ class _FeedPageState extends State<FeedPage> {
                       MusicPlaylistFeedComponent(),
                     ],
                   ),
+
                   ResponsiveContainer(height: 10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -67,94 +69,87 @@ class _FeedPageState extends State<FeedPage> {
                 ]),
               ),
 
-              ResponsiveContainer(
-                width: double.infinity,
-                height: 210,
-                color: backgroundColor,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 23),
-                  child: Column(
-                    children: [
-                      Row(
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 23),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        ResponsiveContainer(
+                          height: 60,
+                          width: 60,
+                          isCubic: true,
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        ResponsiveContainer(width: 5),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            ResponsiveText(text: "Novo lançamento",fontColor: Color(0xff979797),fontSize: 12),
+                            ResponsiveText(text: "Artista",fontWeight: FontWeight.w500,fontColor: Colors.white,fontSize: 16),
+                          ],
+                        )
+                      ],
+                    ),
+                    ResponsiveContainer(height: 18),
+                    ResponsiveContainer(
+                      height: 130,
+                      width: 326,
+                      color: DefaultPlaceholder.backgroundColor,
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      child: Stack(
                         children: [
-                          ResponsiveContainer(
-                            height: 60,
-                            width: 60,
-                            isCubic: true,
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          ResponsiveContainer(width: 5),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Row(
                             children: const [
-                              ResponsiveText(text: "Novo lançamento",fontColor: Color(0xff979797),fontSize: 12),
-                              ResponsiveText(text: "Artista",fontWeight: FontWeight.w500,fontColor: Colors.white,fontSize: 16),
+                              ResponsiveContainer(
+                                height: 130,
+                                width: 130,
+                                isCubic: true,
+                                color: Colors.white,
+                                borderRadius: BorderRadius.horizontal(left: Radius.circular(15)),
+                              ),
+                              ResponsiveContainer(width: 12),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ResponsiveContainer(height: 12),
+                                  ResponsiveText(
+                                    text: "Minha música nova", 
+                                    fontColor: Colors.white, 
+                                    fontSize: 16
+                                  ),
+                                  ResponsiveText(
+                                    text: "Single by DJ Onga",
+                                    fontColor: Color(0xff979797), 
+                                    fontSize: 12,
+                                  )
+                                ],
+                              )
                             ],
+                          ),
+                          Positioned(
+                            right: 10,
+                            bottom: 10,
+                            child: IconButton.filled(
+                              style: IconButton.styleFrom(
+                                backgroundColor: Colors.white,
+                              ),
+                              iconSize: 36,
+                              onPressed: (){}, 
+                              icon: Icon(Icons.play_arrow_rounded, color: secondaryColor,size: 36,),
+                            )
                           )
                         ],
+                      
                       ),
-                      ResponsiveContainer(height: 18),
-                      ResponsiveContainer(
-                        height: 130,
-                        width: 326,
-                        color: DefaultPlaceholder.backgroundColor,
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        child: Stack(
-                          children: [
-                            Row(
-                              children: const [
-                                ResponsiveContainer(
-                                  height: 130,
-                                  width: 130,
-                                  isCubic: true,
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.horizontal(left: Radius.circular(15)),
-                                ),
-                                ResponsiveContainer(width: 12),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    ResponsiveContainer(height: 12),
-                                    ResponsiveText(
-                                      text: "Minha música nova", 
-                                      fontColor: Colors.white, 
-                                      fontSize: 16
-                                    ),
-                                    ResponsiveText(
-                                      text: "Single by DJ Onga",
-                                      fontColor: Color(0xff979797), 
-                                      fontSize: 12,
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                            Positioned(
-                              right: 10,
-                              bottom: 10,
-                              child: IconButton.filled(
-                                style: IconButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                ),
-                                iconSize: 36,
-                                onPressed: (){}, 
-                                icon: Icon(Icons.play_arrow_rounded, color: secondaryColor,size: 36,),
-                              )
-                            )
-                          ],
-                        
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
-
-              ResponsiveContainer(height: 10),
-              ResponsiveContainer(
-                width: double.infinity,
-                height: 134,
+              
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: responsiveFigmaHeight(10)),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -190,47 +185,39 @@ class _FeedPageState extends State<FeedPage> {
                 ),
               ),
 
-              ResponsiveContainer(height: 10),
-
-              ResponsiveContainer(height: 10),
-              ResponsiveContainer(
-                width: double.infinity,
-                height: 134,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: responsiveFigmaWidth(23)),
-                      child: ResponsiveText(text: "Tocadas recentemente",fontColor: Colors.white,fontWeight: FontWeight.w700, fontSize: 16),
-                    ),
-                    ResponsiveContainer(height: 8),
-                    ResponsiveContainer(
-                      height: 100,
-                      width: double.infinity,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            ResponsiveContainer(width: 23),
-                            for(int i = 0; i < 5; i++)
-                              Padding(
-                                padding: EdgeInsets.only(right: responsiveFigmaWidth(10)),
-                                child: ResponsiveContainer(
-                                  height: 100,
-                                  width: 100,
-                                  isCubic: true,
-                                  borderRadius: BorderRadius.circular(15),
-                                  color: Colors.white,
-                                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: responsiveFigmaWidth(23)),
+                    child: ResponsiveText(text: "Tocadas recentemente",fontColor: Colors.white,fontWeight: FontWeight.w700, fontSize: 16),
+                  ),
+                  ResponsiveContainer(height: 8),
+                  ResponsiveContainer(
+                    height: 100,
+                    width: double.infinity,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          ResponsiveContainer(width: 23),
+                          for(int i = 0; i < 5; i++)
+                            Padding(
+                              padding: EdgeInsets.only(right: responsiveFigmaWidth(10)),
+                              child: ResponsiveContainer(
+                                height: 100,
+                                width: 100,
+                                isCubic: true,
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
                               ),
-                          ],
-                        ),
+                            ),
+                        ],
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
-
             ],
           ),
         ));
