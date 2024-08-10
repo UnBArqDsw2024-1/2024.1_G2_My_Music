73fad9f3fc72c674b2ef5d3b753cc0ae629c93b5
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-09 23:31:28 -0300

    Profile Drawer criado na FeedPage básica

---
+++ my_music_code/lib/Profile/edit_profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
