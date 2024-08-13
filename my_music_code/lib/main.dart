b7865145c30989355313d2e559d0997c80469a5d
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-13 14:25:13 -0300

    solving main

--- my_music_code/lib/main.dart
+++ my_music_code/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:my_music_code/Auth/auth_page.dart';
 import 'package:my_music_code/Globals/size_config.dart';
 import 'package:my_music_code/Globals/style.dart';
-import 'package:my_music_code/MyPlaylists/playlist_page.dart';
 import 'package:my_music_code/firebase_options.dart';
 
 void main() async {
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: false,
       ),
-      home: MyPlaylistPage(),
+      home: AuthPage(),
     );
   }
 }
