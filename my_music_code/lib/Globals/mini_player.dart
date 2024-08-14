814b9a33f57334642739e529c0df3d00b9bf9078
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-13 23:11:36 -0300

    mini player padding add

--- my_music_code/lib/Globals/mini_player.dart
+++ my_music_code/lib/Globals/mini_player.dart
@@ -14,38 +14,44 @@ class MiniPlayer extends StatefulWidget {
 class _MiniPlayerState extends State<MiniPlayer> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context) => FeedMusicPage()));
-      },
-      child: Container(
-        color: secondaryColor,
-        height: 80, 
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: DecorationImage(image: NetworkImage(DefaultPlaceholder.image))
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => FeedMusicPage()));
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: secondaryColor,
+            borderRadius: BorderRadius.circular(5)
+          ),
+          height: 80, 
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    image: DecorationImage(image: NetworkImage(DefaultPlaceholder.image))
+                  ),
+                  height: 64, width: 64,
                 ),
-                height: 64, width: 64,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Música legal", style: TextStyle(color: primaryFontColor, fontSize: 16),),
-                  Text("Luísa Sonza", style: TextStyle(color: secondaryFontColor, fontSize: 12),)
-                ],
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Música legal", style: TextStyle(color: primaryFontColor, fontSize: 16),),
+                    Text("Luísa Sonza", style: TextStyle(color: secondaryFontColor, fontSize: 12),)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
