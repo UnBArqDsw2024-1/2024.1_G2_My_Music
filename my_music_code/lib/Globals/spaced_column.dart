5334cbfb20143f96eda3718558a3608a1d8d8dc9
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-14 15:30:03 -0300

    About page components

--- my_music_code/lib/Globals/spaced_column.dart
+++ my_music_code/lib/Globals/spaced_column.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:my_music_code/Globals/responsive_container.dart';
 
 class SpacedColumn extends StatefulWidget {
-  const SpacedColumn({super.key, required this.children, this.spacing, this.padding, this.mainAxisSize});
+  const SpacedColumn({super.key, required this.children, this.spacing, this.padding, this.mainAxisSize, this.crossAxisAlignment});
   final List<Widget> children;
   final double? spacing;
   final EdgeInsets? padding;
   final MainAxisSize? mainAxisSize;
+  final CrossAxisAlignment? crossAxisAlignment;
   @override
   State<SpacedColumn> createState() => _SpacedColumnState();
 }
@@ -17,6 +18,7 @@ class _SpacedColumnState extends State<SpacedColumn> {
     return Padding(
         padding: widget.padding ?? EdgeInsets.zero,
         child: Column(
+          crossAxisAlignment: widget.crossAxisAlignment ?? CrossAxisAlignment.center,
             mainAxisSize: widget.mainAxisSize ?? MainAxisSize.min,
             children: List.generate(widget.children.length, (index) {
               return Column(
--- my_music_code/lib/legal/about_page.dart
+++ my_music_code/lib/legal/about_page.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:my_music_code/Globals/spaced_column.dart';
 import 'package:my_music_code/Globals/style.dart';
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.title, required this.description});
+  final String title, description;
+  @override
+  Widget build(BuildContext context) {
+    return SpacedColumn(
+      spacing: 10,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ],
+    );
+    ;
+  }
+}
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -9,7 +37,6 @@ class AboutPage extends StatefulWidget {
 }
 
 class _AboutPageState extends State<AboutPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,92 +58,34 @@ class _AboutPageState extends State<AboutPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Column(
+          child: SpacedColumn(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [ 
-                Text(
-                'MyMusic',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+            spacing: 20,
+            children: const [
+              TextSection(
+                title: "MyMusic",
+                description: 'My Music é um software de código aberto, desenvolvido para o gerenciamento e reprodução de músicas, inspirado em plataformas robustas como o Spotify. Este projeto foi criado como parte do programa acadêmico da disciplina de Arquitetura e Desenho de Software, no semestre 2024/1, na Universidade de Brasília, Faculdade do Gama, sob a orientação da discente Milene Serrano. O My Music tem como objetivo proporcionar uma experiência prática e interativa, permitindo que os usuários organizem suas playlists, explorem novas músicas e gerenciem seus conteúdos musicais de forma eficiente e personalizada.',
               ),
-              SizedBox(height: 10),
-              Text(
-                'My Music é um software de código aberto, desenvolvido para o gerenciamento e reprodução de músicas, inspirado em plataformas robustas como o Spotify. Este projeto foi criado como parte do programa acadêmico da disciplina de Arquitetura e Desenho de Software, no semestre 2024/1, na Universidade de Brasília, Faculdade do Gama, sob a orientação da discente Milene Serrano. O My Music tem como objetivo proporcionar uma experiência prática e interativa, permitindo que os usuários organizem suas playlists, explorem novas músicas e gerenciem seus conteúdos musicais de forma eficiente e personalizada.',
-                style: TextStyle(color: Colors.white, fontSize: 16),
+              TextSection(
+                title: 'Objetivo', 
+                description: 'O objetivo principal do My Music é proporcionar aos usuários uma ferramenta eficiente e fácil de usar para o gerenciamento de suas músicas. Seja para uso pessoal ou educativo, o projeto está disponível gratuitamente, incentivando a colaboração e o desenvolvimento contínuo da comunidade.',
               ),
-              SizedBox(height: 20),
-              Text(
-                'Objetivo',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+              TextSection(
+                title: 'Licenciamento', 
+                description: 'O código-fonte do My Music está disponível sob a licença MIT, permitindo que qualquer pessoa possa utilizar, modificar e distribuir o software, respeitando os termos da licença. Acreditamos que o compartilhamento de conhecimento e a contribuição coletiva são fundamentais para a evolução da tecnologia.',
               ),
-              SizedBox(height: 10),
-              Text(
-                'O objetivo principal do My Music é proporcionar aos usuários uma ferramenta eficiente e fácil de usar para o gerenciamento de suas músicas. Seja para uso pessoal ou educativo, o projeto está disponível gratuitamente, incentivando a colaboração e o desenvolvimento contínuo da comunidade.',
-                style: TextStyle(color: Colors.white, fontSize: 16),
+              TextSection(
+                title: 'Contribuições', 
+                description: 'Contribuições ao My Music são bem-vindas e incentivadas. Se você deseja participar, fique à vontade para fazer fork do repositório, submeter pull requests, ou sugerir melhorias. Cada contribuição ajuda a fortalecer e aprimorar o projeto.',
               ),
-              SizedBox(height: 20),
-              Text(
-                'Licenciamento',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+              TextSection(
+                title: 'Responsabilidade e Uso', 
+                description: 'O My Music é fornecido "como está", sem garantias de qualquer tipo. Embora façamos o possível para garantir que o software seja funcional e seguro, o uso é por sua conta e risco. Recomendamos que os usuários sigam as melhores práticas ao utilizar e modificar o software.',
               ),
-              SizedBox(height: 10),
-              Text(
-                'O código-fonte do My Music está disponível sob a licença MIT, permitindo que qualquer pessoa possa utilizar, modificar e distribuir o software, respeitando os termos da licença. Acreditamos que o compartilhamento de conhecimento e a contribuição coletiva são fundamentais para a evolução da tecnologia.',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Contribuições',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Contribuições ao My Music são bem-vindas e incentivadas. Se você deseja participar, fique à vontade para fazer fork do repositório, submeter pull requests, ou sugerir melhorias. Cada contribuição ajuda a fortalecer e aprimorar o projeto.',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Responsabilidade e Uso',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'O My Music é fornecido "como está", sem garantias de qualquer tipo. Embora façamos o possível para garantir que o software seja funcional e seguro, o uso é por sua conta e risco. Recomendamos que os usuários sigam as melhores práticas ao utilizar e modificar o software.',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Desenvolvimento Contínuo',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Este projeto está em constante evolução, com atualizações e melhorias sendo feitas regularmente. Acompanhe o repositório oficial no GitHub para ficar por dentro das últimas novidades e colabore conosco para fazer do My Music uma ferramenta cada vez melhor.',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
+              TextSection(
+                title: 'Desenvolvimento Contínuo', 
+                description: 'Este projeto está em constante evolução, com atualizações e melhorias sendo feitas regularmente. Acompanhe o repositório oficial no GitHub para ficar por dentro das últimas novidades e colabore conosco para fazer do My Music uma ferramenta cada vez melhor.',
+              )
             ],
           ),
         ),
