441964d9c3c03854190417b603522977100397d6
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-14 12:30:50 -0300

    Update user_page_of_playlists.dart

--- my_music_code/lib/MyPlaylists/user_page_of_playlists.dart
+++ my_music_code/lib/MyPlaylists/user_page_of_playlists.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_music_code/Globals/style.dart';
 import 'package:my_music_code/MyPlaylists/new_playlist_page.dart';
