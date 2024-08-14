ba9019c7a4b694d56ed1a6a6b76860efcd12a4a5
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-14 15:37:42 -0300

    About e textSection modificados

--- my_music_code/lib/legal/about_page.dart
+++ my_music_code/lib/legal/about_page.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:my_music_code/Globals/spaced_column.dart';
 import 'package:my_music_code/Globals/style.dart';
+import 'package:my_music_code/legal/text_section.dart';
 
-class TextSection extends StatelessWidget {
-  const TextSection({super.key, required this.title, required this.description});
-  final String title, description;
-  @override
-  Widget build(BuildContext context) {
-    return SpacedColumn(
-      spacing: 10,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        Text(
-          description,
-          style: TextStyle(color: Colors.white, fontSize: 16),
-        ),
-      ],
-    );
-    ;
+Map<String, Map<String,String>> aboutMapText = {
+  "MyMusic":{
+    "title":"MyMusic",
+    "description":'My Music é um software de código aberto, desenvolvido para o gerenciamento e reprodução de músicas, inspirado em plataformas robustas como o Spotify. Este projeto foi criado como parte do programa acadêmico da disciplina de Arquitetura e Desenho de Software, no semestre 2024/1, na Universidade de Brasília, Faculdade do Gama, sob a orientação da discente Milene Serrano. O My Music tem como objetivo proporcionar uma experiência prática e interativa, permitindo que os usuários organizem suas playlists, explorem novas músicas e gerenciem seus conteúdos musicais de forma eficiente e personalizada.',
+  },
+  "Objetivo":{
+    "title":"Objetivo",
+    "description":'O objetivo principal do My Music é proporcionar aos usuários uma ferramenta eficiente e fácil de usar para o gerenciamento de suas músicas. Seja para uso pessoal ou educativo, o projeto está disponível gratuitamente, incentivando a colaboração e o desenvolvimento contínuo da comunidade.',
+  },
+  'Licenciamento':{
+    "title":"Licenciamento",
+    'description':'O código-fonte do My Music está disponível sob a licença MIT, permitindo que qualquer pessoa possa utilizar, modificar e distribuir o software, respeitando os termos da licença. Acreditamos que o compartilhamento de conhecimento e a contribuição coletiva são fundamentais para a evolução da tecnologia.',
+  },
+  'Contribuições':{
+    "title":"Contribuições",
+    'description':'Contribuições ao My Music são bem-vindas e incentivadas. Se você deseja participar, fique à vontade para fazer fork do repositório, submeter pull requests, ou sugerir melhorias. Cada contribuição ajuda a fortalecer e aprimorar o projeto.',
+  },
+  'Responsabilidade e Uso':{
+    "title":"Responsabilidade e Uso",
+    'description':'O My Music é fornecido "como está", sem garantias de qualquer tipo. Embora façamos o possível para garantir que o software seja funcional e seguro, o uso é por sua conta e risco. Recomendamos que os usuários sigam as melhores práticas ao utilizar e modificar o software.',
+  },
+  'Desenvolvimento Contínuo':{
+    'title':'Desenvolvimento Contínuo',
+    'description':'Este projeto está em constante evolução, com atualizações e melhorias sendo feitas regularmente. Acompanhe o repositório oficial no GitHub para ficar por dentro das últimas novidades e colabore conosco para fazer do My Music uma ferramenta cada vez melhor.',
   }
-}
+};
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -61,32 +62,14 @@ class _AboutPageState extends State<AboutPage> {
           child: SpacedColumn(
             crossAxisAlignment: CrossAxisAlignment.start,
             spacing: 20,
-            children: const [
-              TextSection(
-                title: "MyMusic",
-                description: 'My Music é um software de código aberto, desenvolvido para o gerenciamento e reprodução de músicas, inspirado em plataformas robustas como o Spotify. Este projeto foi criado como parte do programa acadêmico da disciplina de Arquitetura e Desenho de Software, no semestre 2024/1, na Universidade de Brasília, Faculdade do Gama, sob a orientação da discente Milene Serrano. O My Music tem como objetivo proporcionar uma experiência prática e interativa, permitindo que os usuários organizem suas playlists, explorem novas músicas e gerenciem seus conteúdos musicais de forma eficiente e personalizada.',
-              ),
-              TextSection(
-                title: 'Objetivo', 
-                description: 'O objetivo principal do My Music é proporcionar aos usuários uma ferramenta eficiente e fácil de usar para o gerenciamento de suas músicas. Seja para uso pessoal ou educativo, o projeto está disponível gratuitamente, incentivando a colaboração e o desenvolvimento contínuo da comunidade.',
-              ),
-              TextSection(
-                title: 'Licenciamento', 
-                description: 'O código-fonte do My Music está disponível sob a licença MIT, permitindo que qualquer pessoa possa utilizar, modificar e distribuir o software, respeitando os termos da licença. Acreditamos que o compartilhamento de conhecimento e a contribuição coletiva são fundamentais para a evolução da tecnologia.',
-              ),
-              TextSection(
-                title: 'Contribuições', 
-                description: 'Contribuições ao My Music são bem-vindas e incentivadas. Se você deseja participar, fique à vontade para fazer fork do repositório, submeter pull requests, ou sugerir melhorias. Cada contribuição ajuda a fortalecer e aprimorar o projeto.',
-              ),
-              TextSection(
-                title: 'Responsabilidade e Uso', 
-                description: 'O My Music é fornecido "como está", sem garantias de qualquer tipo. Embora façamos o possível para garantir que o software seja funcional e seguro, o uso é por sua conta e risco. Recomendamos que os usuários sigam as melhores práticas ao utilizar e modificar o software.',
-              ),
-              TextSection(
-                title: 'Desenvolvimento Contínuo', 
-                description: 'Este projeto está em constante evolução, com atualizações e melhorias sendo feitas regularmente. Acompanhe o repositório oficial no GitHub para ficar por dentro das últimas novidades e colabore conosco para fazer do My Music uma ferramenta cada vez melhor.',
+            children: List.from(
+              aboutMapText.entries.map(
+                (entry) => TextSection(
+                  title: entry.value['title']!,
+                  description: entry.value['description']!,
+                )
               )
-            ],
+            )
           ),
         ),
       ),
