a5ffbe43d2c95f1a2f27ba63aa1354c7aca56e29
LimirioGuimaraes <limirioguimaraes@gmail.com>
2024-08-15 13:03:25 -0300

    atualizações navegação

--- my_music_code/lib/MyPlaylists/user_page_of_playlists.dart
+++ my_music_code/lib/MyPlaylists/user_page_of_playlists.dart
@@ -65,7 +65,6 @@ class _UserPageOfPlaylistsState extends State<UserPageOfPlaylists> {
           image: album.images!.isNotEmpty ? album.images!.first.url! : DefaultPlaceholder.image,
         );
       } catch (e) {
-        // print('Erro ao obter o álbum com ID $id: $e');
         // retorna modelo em branco
         return AlbumModel(
           id: id,
@@ -78,7 +77,6 @@ class _UserPageOfPlaylistsState extends State<UserPageOfPlaylists> {
 
     _generateAlbumWidgets();
   }
-
 
   void _generatePlaylistWidgets() {
     playlistWidgets = playlists.map((playlist) {
@@ -145,74 +143,74 @@ class _UserPageOfPlaylistsState extends State<UserPageOfPlaylists> {
     }).toList();
   }
 
-  void _generateAlbumWidgets() {
-    albumWidgets = albums.map((album) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 0),
-        child: InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MyAlbumPage(album: album)),
-            );
-          },
-          splashColor: primaryColor.withOpacity(0.3),
-          highlightColor: primaryColor.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-            height: 120,
-            padding: EdgeInsets.all(7.5),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 15, left: 7.5),
-                  child: Container(
-                    height: 105,
-                    width: 105,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.black,
-                      image: DecorationImage(
-                        image: NetworkImage(album.image),
-                        fit: BoxFit.cover,
-                      ),
+void _generateAlbumWidgets() {
+  albumWidgets = albums.map((album) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0),
+      child: InkWell(
+        onTap: () {
+          
+          // navegação aqui
+          
+        },
+        splashColor: primaryColor.withOpacity(0.3),
+        highlightColor: primaryColor.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          height: 120,
+          padding: EdgeInsets.all(7.5),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 15, left: 7.5),
+                child: Container(
+                  height: 105,
+                  width: 105,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.black,
+                    image: DecorationImage(
+                      image: NetworkImage(album.image),
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      album.name,
-                      style: TextStyle(
-                        color: primaryFontColor,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 24,
-                      ),
-                      overflow: TextOverflow.ellipsis,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    album.name,
+                    style: TextStyle(
+                      color: primaryFontColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24,
                     ),
-                    Text(
-                      album.artist,
-                      style: TextStyle(
-                        color: secondaryFontColor,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                      ),
-                      overflow: TextOverflow.ellipsis,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    album.artist,
+                    style: TextStyle(
+                      color: secondaryFontColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
                     ),
-                  ],
-                ),
-              ],
-            ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-      );
-    }).toList();
+      ),
+    );
+  }).toList();
 
-    setState(() {
-      // Re-renderizar a tela para mostrar os álbuns após a lista ser atualizada
-    });
-  }
+  setState(() {
+    // Re-renderizar a tela para mostrar os álbuns após a lista ser atualizada
+  });
+}
+
 
   void _filterList() {
     setState(() {
