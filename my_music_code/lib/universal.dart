455499a82b0e717b14921a52cb8de50d4bea950f
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-14 22:56:03 -0300

    Update universal.dart

--- my_music_code/lib/universal.dart
+++ my_music_code/lib/universal.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_music_code/Feed/Components/feed_music_grid.dart';
 
 AudioPlayer audioPlayer = AudioPlayer();
-Music pastMusic = Music();
 Music currentMusic = Music();
 User? user;
 
