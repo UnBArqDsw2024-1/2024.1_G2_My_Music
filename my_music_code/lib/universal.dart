a37bf2c825dbc2777b6a863072bd172e09c866b8
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-16 17:13:21 -0300

    currentAlbum implement

--- my_music_code/lib/universal.dart
+++ my_music_code/lib/universal.dart
@@ -2,6 +2,7 @@ library universal;
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_music_code/Models/album_model.dart';
 import 'package:my_music_code/Models/music_model.dart';
 import 'package:my_music_code/Models/sign_login_model.dart';
 import 'package:spotify/spotify.dart' hide User;
@@ -14,3 +15,4 @@ List<Music> releaseListMusics = List.empty(growable: true);
 List<Music> currentListMusic = List.empty(growable: true);
 List<Music> currentListMusicShuffle = List.empty(growable: true);
 SignLoginModel userModel = SignLoginModel();
+AlbumModel currentAlbum = AlbumModel();
