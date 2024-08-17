8082c8ee2a7496cb67339c0d87e73f610bade0cd
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-08-16 21:17:42 -0300

    Update main.dart

--- my_music_code/lib/main.dart
+++ my_music_code/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_music_code/Auth/auth_page.dart';
 // import 'package:my_music_code/Globals/navigator_page.dart';
 import 'package:my_music_code/Globals/size_config.dart';
@@ -11,7 +12,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
