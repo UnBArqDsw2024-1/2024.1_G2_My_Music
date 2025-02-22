59290bbf14308bde43900c44606ec50544f105bd
Lucas Alves Vilela <63984060+Lucas-AV@users.noreply.github.com>
2025-02-22 14:33:20 -0300

    Update api_settings.dart
    Trocando chaves da API

--- my_music_code/lib/SpotifyApi/api_settings.dart
+++ my_music_code/lib/SpotifyApi/api_settings.dart
@@ -1,4 +1,4 @@
 class ApiSettings {
-  static const String clientId = '220af7a477634e129a3e920e89f65e5c';
-  static const String clientSecret = '2b8275937d8f4888a663ae9619d042bb';
+  static const String clientId = '220af7a477634e129a3e920e89f65e5c'; // Depreciado - Para testar coloque sua chave de API disponível em: https://developer.spotify.com/
+  static const String clientSecret = '2b8275937d8f4888a663ae9619d042bb'; // Depreciado - Para testar coloque sua chave de API: https://developer.spotify.com/
 }
